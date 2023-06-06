3096d8b15a97aed2d7753c28959d7edd638162f5
nguyentiendat <tiendatnguyenit@gmail.com>
2023-06-06 23:29:24 +0700

    Create project

---
+++ src/assets/images/index.js
@@ -0,0 +1,10 @@
+export default {
+    allCheck: require('./resources/allCheck.png'),
+    checked: require('./resources/checked.png'),
+    deleteItem: require('./resources/deleteItem.png'),
+    edit: require('./resources/edit.png'),
+    starChecked: require('./resources/starChecked.png'),
+    starUncheck: require('./resources/starUncheck.png'),
+    uncheck: require('./resources/uncheck.png'),
+    updateCheck: require('./resources/updateCheck.png'),
+}
